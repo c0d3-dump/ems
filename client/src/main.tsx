c4eb332e5accaafae37775e1cf4d19708800f26f
c0d3-dump <c0d3.dump@gmail.com>
2023-12-01 15:02:01 +0530

    removing redux

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { Provider } from "react-redux";
-import { store } from "./redux/base/store";
 import axios from "axios";
 import { toast } from "./components/ui/use-toast.ts";
 import { Auth0Provider } from "@auth0/auth0-react";
@@ -32,9 +30,8 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
       }}
       cacheLocation="localstorage"
     >
-      <Provider store={store}>
-        <App />
-      </Provider>
+      =
+      <App />=
     </Auth0Provider>
   </React.StrictMode>
 );
